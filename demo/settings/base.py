a165199367a16e8ad2be3d8232d81deca0f0c943
daniel <bro.danielx@gmail.com>
2020-04-08 07:10:21 -0400

    add command to rename project

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
             os.path.dirname(
@@ -19,11 +21,7 @@ BASE_DIR = os.path.dirname(
                 os.path.abspath(__file__))))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^9zc5zu(w29%%_tpxe@j-ts(lclf^vovs8^sq)yburt-^f%om7'
+SECRET_KEY = config('SECRET_KEY')
 
 
 INSTALLED_APPS = [
@@ -33,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
 ]
 
 MIDDLEWARE = [
